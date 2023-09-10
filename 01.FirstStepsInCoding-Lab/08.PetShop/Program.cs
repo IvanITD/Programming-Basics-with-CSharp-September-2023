25eede928950eb18d9e1e1b33576a8b9b2fd0848
IvanITD <ivan78608@gmail.com>
2023-09-11 00:55:21 +0300

    Another resolved task 08. Pet Shop

---
+++ 01.FirstStepsInCoding-Lab/08.PetShop/Program.cs
@@ -0,0 +1,12 @@
+﻿int dogFood = int.Parse(Console.ReadLine());
+int catFood = int.Parse(Console.ReadLine());
+
+double oneDogPack = 2.50;
+double oneCatPack = 4.0;
+
+double totalDogFoodPrice = dogFood * oneDogPack;
+double totalCatFoodPrice = catFood * oneCatPack;
+
+double totalExpence = totalDogFoodPrice + totalCatFoodPrice;
+
+Console.WriteLine($"{totalExpence} lv.");

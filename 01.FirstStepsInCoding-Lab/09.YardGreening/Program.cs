ce0277bcf9dc09d623f5ffdf3cc5aad98d63e8ca
IvanITD <ivan78608@gmail.com>
2023-09-11 01:16:30 +0300

    Another Resolved Task 09. Yard Greening

---
+++ 01.FirstStepsInCoding-Lab/09.YardGreening/Program.cs
@@ -0,0 +1,11 @@
+﻿double squareMeters = double.Parse(Console.ReadLine());
+
+double perOneSquareMeter = 7.61;
+double discount = 0.18;
+
+double totalGreeningPrice = squareMeters * perOneSquareMeter;
+double discountedPrice = discount * totalGreeningPrice;
+double finalPrice = totalGreeningPrice - discountedPrice;
+
+Console.WriteLine($"The final price is: {finalPrice} lv.");
+Console.WriteLine($"The discount is: {discountedPrice} lv.");

f7dc42bbd72d17c0058be50a6ede74231192c686
IvanITD <ivan78608@gmail.com>
2023-09-11 00:53:57 +0300

    Another resolved task 07. Projects Creation

---
+++ 01.FirstStepsInCoding-Lab/07.ProjectsCreation/Program.cs
@@ -0,0 +1,9 @@
+﻿string architectName = Console.ReadLine();
+int projectAmmount = int.Parse(Console.ReadLine());
+
+int timePerProject = 3;
+
+int neededHours = projectAmmount * timePerProject;
+
+Console.WriteLine($"The architect {architectName} will need {neededHours} " +
+    $"hours to complete {projectAmmount} project/s.");

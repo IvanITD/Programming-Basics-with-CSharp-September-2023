329f9cdc170ad9dc6f9bf4153913ac02b3db1d37
IvanITD <ivan78608@gmail.com>
2023-09-13 04:24:22 +0300

    Another resolved task 04. Vacation Books List

---
+++ 02-FirstStepsInCoding-Exercise/04.VacationBooksList/Program.cs
@@ -0,0 +1,8 @@
+﻿int pageAmmount = int.Parse(Console.ReadLine());
+int pagesPerHour = int.Parse(Console.ReadLine());
+int daysAmmount = int.Parse(Console.ReadLine());
+
+int neededHoursToRead = pageAmmount / pagesPerHour;
+int neededHoursPerDay = neededHoursToRead / daysAmmount;
+
+Console.WriteLine(neededHoursPerDay);

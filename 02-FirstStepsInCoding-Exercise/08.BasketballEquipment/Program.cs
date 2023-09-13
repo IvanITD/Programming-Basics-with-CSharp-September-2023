8286a238f111e197ccb9d048f91f110991004328
IvanITD <ivan78608@gmail.com>
2023-09-13 13:36:48 +0300

    Another resolved task 08. Basketball Equipment

---
+++ 02-FirstStepsInCoding-Exercise/08.BasketballEquipment/Program.cs
@@ -0,0 +1,10 @@
+﻿int basketballYearSubscription = int.Parse(Console.ReadLine());
+
+double shoes = basketballYearSubscription - (basketballYearSubscription * 0.40);
+double outfit = shoes - (shoes * 0.20);
+double basketball = outfit / 4;
+double accessories = basketball / 5;
+
+double taxPerYear = basketballYearSubscription + shoes + outfit + basketball + accessories;
+
+Console.WriteLine(taxPerYear);

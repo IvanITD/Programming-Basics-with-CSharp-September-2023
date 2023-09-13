3d348933b0db7e4b16578f85cfd0cd374fbebeaa
IvanITD <ivan78608@gmail.com>
2023-09-13 14:27:15 +0300

    Another resolved task 01.ExcellentResult

--- 03-ConditionalStatements-Lab/01.ExcellentResult/Program.cs
+++ 03-ConditionalStatements-Lab/01.ExcellentResult/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double grade = double.Parse(Console.ReadLine());
+
+if (grade >= 5.50 == grade <= 6)
+{
+    Console.WriteLine("Excellent!");
+}

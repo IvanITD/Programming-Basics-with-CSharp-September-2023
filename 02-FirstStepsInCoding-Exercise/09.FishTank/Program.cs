ce162d662e617962a0b7e0fd0ab5ee9da8f11f81
IvanITD <ivan78608@gmail.com>
2023-09-13 14:18:07 +0300

    Another resolved task 09. Fish Tank

---
+++ 02-FirstStepsInCoding-Exercise/09.FishTank/Program.cs
@@ -0,0 +1,14 @@
+﻿int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int  height = int.Parse(Console.ReadLine());
+double percent = double.Parse(Console.ReadLine());
+
+double waterLiter = length * width * height;
+
+double waterVolumeLiter = waterLiter * 0.001;
+
+double usedSpace = percent / 100;
+
+double neededLiters = waterVolumeLiter * (1 - usedSpace);
+
+Console.WriteLine(neededLiters);

36dc2b583e2b4e125548174a1c5c2221ea2b1134
IvanITD <ivan78608@gmail.com>
2023-09-13 13:27:00 +0300

    Another resolved task 07. Food Delivery

---
+++ 02-FirstStepsInCoding-Exercise/07.FoodDelivery/Program.cs
@@ -0,0 +1,16 @@
+﻿int chickenMenuAmount = int.Parse(Console.ReadLine());
+int fishMenuAmount = int.Parse(Console.ReadLine());
+int vegeterianMenuAmount = int.Parse(Console.ReadLine());
+
+double chickenMenuPrice = chickenMenuAmount * 10.35;
+double fishMenuPrice = fishMenuAmount * 12.40;
+double vegeterianMenuPrice = vegeterianMenuAmount * 8.15;
+
+double totalPricing = chickenMenuPrice + fishMenuPrice + vegeterianMenuPrice;
+double withDesert = totalPricing * 0.20;
+
+double delivery = 2.50;
+
+double finalPrice = totalPricing + withDesert + delivery;
+
+Console.WriteLine(finalPrice);

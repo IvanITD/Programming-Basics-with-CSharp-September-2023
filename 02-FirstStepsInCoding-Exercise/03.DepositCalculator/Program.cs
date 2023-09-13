190e288c13d2b934575ebe04468c07cadd99ffe3
IvanITD <ivan78608@gmail.com>
2023-09-13 04:06:37 +0300

    Another resolved task 03. Deposit Calculator

---
+++ 02-FirstStepsInCoding-Exercise/03.DepositCalculator/Program.cs
@@ -0,0 +1,7 @@
+﻿double deposit = double.Parse(Console.ReadLine());
+int term = int.Parse(Console.ReadLine());
+double AnualInterestRate = double.Parse(Console.ReadLine());
+
+double sum = deposit + term * ((deposit * (AnualInterestRate / 100)) / 12);
+
+Console.WriteLine(sum);

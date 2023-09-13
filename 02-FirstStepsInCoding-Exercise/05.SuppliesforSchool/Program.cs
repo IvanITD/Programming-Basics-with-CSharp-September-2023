1b9b726e6997b3ec3d6376423bf08a108c55bdcd
IvanITD <ivan78608@gmail.com>
2023-09-13 04:48:30 +0300

    Another resolved task 05. Supplies for School

---
+++ 02-FirstStepsInCoding-Exercise/05.SuppliesforSchool/Program.cs
@@ -0,0 +1,17 @@
+﻿int penPacketAmount = int.Parse(Console.ReadLine());
+int markerPacketAmount  = int.Parse(Console.ReadLine());
+int cleaningDetergentAmount = int.Parse(Console.ReadLine());
+int discountPercent = int.Parse(Console.ReadLine());
+
+
+double penPacketPrice = penPacketAmount * 5.80;
+double markerPacketPrice = markerPacketAmount * 7.20;
+double cleaningDetergentPricePerLiter = cleaningDetergentAmount * 1.20;
+
+double totalPrice = penPacketPrice + markerPacketPrice + cleaningDetergentPricePerLiter;
+
+double discount = (double)discountPercent / 100;
+
+double result = totalPrice - (totalPrice * discount);
+
+Console.WriteLine(result);

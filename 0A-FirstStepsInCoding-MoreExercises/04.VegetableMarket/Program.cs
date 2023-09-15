9894dfaf7d8e863e5facdd615972d31d520addf2
IvanITD <ivan78608@gmail.com>
2023-09-15 04:26:46 +0300

    Another resolved task 04. Vegetable Market

---
+++ 0A-FirstStepsInCoding-MoreExercises/04.VegetableMarket/Program.cs
@@ -0,0 +1,15 @@
+﻿double vegetablePricePerKg = double.Parse(Console.ReadLine());
+double fruitPricePerKg = double.Parse(Console.ReadLine());
+double totalVegetableKg = double.Parse(Console.ReadLine());
+double totalFruitKg = double.Parse(Console.ReadLine());
+
+double oneEuroInBgn = 1.94;
+
+double totalVegetablePrice = vegetablePricePerKg * totalVegetableKg;
+double totalFruitPrice = fruitPricePerKg * totalFruitKg;
+
+double income = totalVegetablePrice + totalFruitPrice;
+
+double incomeInEuro = income / oneEuroInBgn;
+
+Console.WriteLine($"{incomeInEuro:F2}");

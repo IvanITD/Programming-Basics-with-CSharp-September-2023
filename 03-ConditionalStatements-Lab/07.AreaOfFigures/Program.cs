6bf83928757b7537fe3d784c45237515cc02d410
IvanITD <ivan78608@gmail.com>
2023-09-15 03:39:39 +0300

    04 - Conditional Statements - Exercise

---
+++ 03-ConditionalStatements-Lab/07.AreaOfFigures/Program.cs
@@ -0,0 +1,32 @@
+﻿string figureType = Console.ReadLine();
+
+if (figureType == "square")
+{
+    double length = double.Parse(Console.ReadLine());
+    double area = length * length;
+
+    Console.WriteLine($"{area:F3}");
+}
+else if (figureType == "rectangle")
+{
+    double lengthA = double.Parse(Console.ReadLine());
+    double lengthB = double.Parse(Console.ReadLine());
+    double area = lengthA * lengthB;
+
+    Console.WriteLine($"{area:F3}");
+}
+else if (figureType == "circle")
+{
+    double radius = double.Parse(Console.ReadLine());
+    double area = Math.PI * radius * radius;
+
+    Console.WriteLine($"{area:F3}");
+}
+else if (figureType == "triangle")
+{
+    double lengthA = double.Parse(Console.ReadLine());
+    double lengthB = double.Parse(Console.ReadLine());
+    double area = lengthA * lengthB / 2;
+
+    Console.WriteLine($"{area:F3}");
+}

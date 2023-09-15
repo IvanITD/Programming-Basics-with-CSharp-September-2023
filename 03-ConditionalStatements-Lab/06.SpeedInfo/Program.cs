759b6dbe0bd506bdea836c01dc208240453e41d0
IvanITD <ivan78608@gmail.com>
2023-09-15 03:23:47 +0300

    Another resolved task 06. Speed Info

---
+++ 03-ConditionalStatements-Lab/06.SpeedInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿double pace = double.Parse(Console.ReadLine());
+
+if (pace <= 10)
+{
+    Console.WriteLine("slow");
+}
+else if (pace > 10 && pace <= 50)
+{
+    Console.WriteLine("average");
+}
+else if (pace > 50 && pace <= 150)
+{
+    Console.WriteLine("fast");
+}
+else if (pace > 150 && pace <= 1000)
+{
+    Console.WriteLine("ultra fast");
+}
+else
+{
+    Console.WriteLine("extremely fast");
+}

279fbc3072e947e1108adf6430105fcc765f441c
IvanITD <ivan78608@gmail.com>
2023-09-20 03:08:27 +0300

    Another resolved task 01. Sum Seconds

--- 04-ConditionalStatements-Exercise/01.SumSeconds/Program.cs
+++ 04-ConditionalStatements-Exercise/01.SumSeconds/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int firstTime = int.Parse(Console.ReadLine());
+int secondTime = int.Parse(Console.ReadLine());
+int thirdTime = int.Parse(Console.ReadLine());
+
+int totalTime = firstTime + secondTime + thirdTime;
+
+int timeInMinutes = totalTime / 60;
+int timeInSeconds = totalTime % 60;
+
+Console.WriteLine($"{timeInMinutes}:{timeInSeconds:D2}");

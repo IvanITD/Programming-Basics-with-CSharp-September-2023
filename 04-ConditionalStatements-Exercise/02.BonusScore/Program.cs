61918be5e132ac840c2a2daad2ef8563927c5a63
IvanITD <ivan78608@gmail.com>
2023-09-20 23:04:34 +0300

    Another resolved task 02. Bonus Score

---
+++ 04-ConditionalStatements-Exercise/02.BonusScore/Program.cs
@@ -0,0 +1,64 @@
+﻿int startingPoints = int.Parse(Console.ReadLine());
+
+double bonusPoints = 0.0;
+
+if (startingPoints <= 100)
+{
+    bonusPoints += 5;
+
+    if (startingPoints % 2 == 0)
+    {
+        bonusPoints++;
+
+    }
+    
+    if (startingPoints % 10 == 5)
+    {
+        bonusPoints += 2;
+    }
+
+    double totalPoints = startingPoints + bonusPoints;
+
+    Console.WriteLine(bonusPoints);
+    Console.WriteLine(totalPoints);
+}
+else if (startingPoints > 100 && startingPoints <= 1000)
+{
+    bonusPoints = startingPoints * 0.20;
+
+    if (startingPoints % 2 == 0)
+    {
+        bonusPoints++;
+
+    }
+    
+    if (startingPoints % 10 == 5)
+    {
+        bonusPoints += 2;
+    }
+
+    double totalPoints = startingPoints + bonusPoints;
+
+    Console.WriteLine(bonusPoints);
+    Console.WriteLine(totalPoints);
+}
+else if (startingPoints > 1000)
+{
+    bonusPoints = startingPoints * 0.10;
+
+    if (startingPoints % 2 == 0)
+    {
+        bonusPoints++;
+
+    }
+    
+    if (startingPoints % 10 == 5)
+    {
+        bonusPoints += 2;
+    }
+
+    double totalPoints = startingPoints + bonusPoints;
+
+    Console.WriteLine(bonusPoints);
+    Console.WriteLine(totalPoints);
+}

f51330f31261c2b5a0569c71b120d64cd90442e1
IvanITD <ivan78608@gmail.com>
2023-09-24 02:46:18 +0300

    Another resolved task 03. Time + 15 Minutes

---
+++ 04-ConditionalStatements-Exercise/03.TimeAnd15Minutes/Program.cs
@@ -0,0 +1,26 @@
+﻿int inputHours = int.Parse(Console.ReadLine());
+int inputMinutes = int.Parse(Console.ReadLine());
+
+int hoursToMinuts = inputHours * 60;
+
+int totalMinuteTime = hoursToMinuts + inputMinutes;
+
+totalMinuteTime += 15; // The 15 minutes from the assigment
+
+int hours = 0;
+
+int minutes = totalMinuteTime % 60;
+hours = totalMinuteTime / 60;
+
+if (hours == 24)
+{
+    hours = 0;
+   
+    if (minutes == 60)
+    {
+        hours++;
+        minutes = 0;
+    }
+}
+
+Console.WriteLine($"{hours}:{minutes:D2}");

18b23d724c3b05588e570bd68c18083cc83e4826
IvanITD <ivan78608@gmail.com>
2023-09-25 01:42:42 +0300

    Another resolved task 05. Godzilla vs. Kong

---
+++ 04-ConditionalStatements-Exercise/05.GodzillaVsKong/Program.cs
@@ -0,0 +1,33 @@
+﻿double movieBudget = double.Parse(Console.ReadLine());
+int peopleAmount = int.Parse(Console.ReadLine());
+double clothingPricePerOnePerson = double.Parse(Console.ReadLine());
+
+double decorationPrice = movieBudget * 0.10;
+double clothingPrice = peopleAmount * clothingPricePerOnePerson;
+
+double discountforClothing = 0.0;
+
+if (peopleAmount > 150)
+{
+    discountforClothing = clothingPrice * 0.10;
+}
+
+double totalClothing = clothingPrice - discountforClothing;
+
+double totalPrice = decorationPrice + totalClothing;
+
+
+double neededMoney = movieBudget - totalPrice;
+double remainingMoney = totalPrice - movieBudget;
+
+
+if (totalPrice > movieBudget)
+{
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {Math.Abs(neededMoney):F2} leva more.");
+}
+else if (totalPrice <= movieBudget)
+{
+    Console.WriteLine("Action!");
+    Console.WriteLine($"Wingard starts filming with {Math.Abs(remainingMoney):F2} leva left.");
+}

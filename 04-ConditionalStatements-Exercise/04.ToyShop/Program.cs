7f59e9ffb65aebb50c2e4d6799fe8c295d26b026
IvanITD <ivan78608@gmail.com>
2023-09-24 03:47:13 +0300

    The task is resolved, but it shows 90/100 in Judge Still has to be fixed!!!

---
+++ 04-ConditionalStatements-Exercise/04.ToyShop/Program.cs
@@ -0,0 +1,49 @@
+﻿
+double priceForExcursion = double.Parse(Console.ReadLine());
+int puzzleAmount = int.Parse(Console.ReadLine());
+int talkingDollsAmount = int.Parse(Console.ReadLine());
+int tedyBearAmount = int.Parse(Console.ReadLine());
+int minionsAmount = int.Parse(Console.ReadLine());
+int trucksAmount = int.Parse(Console.ReadLine());
+
+int totalToys = puzzleAmount + talkingDollsAmount + tedyBearAmount + minionsAmount + trucksAmount;
+
+double puzzlePrice = puzzleAmount * 2.60;
+double talkingDollPrice = talkingDollsAmount * 3;
+double tedyBearPrice = tedyBearAmount * 4.10;
+double minionsPrice = minionsAmount * 8.20;
+double truckPrice = trucksAmount * 2;
+
+double totalPrice = puzzlePrice + talkingDollPrice + tedyBearPrice + minionsPrice + truckPrice;
+
+if (totalToys >= 50)
+{
+    double discount = totalPrice * 0.25;
+    double discountFromTotalPrice = totalPrice - discount;
+
+    double rentPrice = discountFromTotalPrice * 0.10;
+    double profit = discountFromTotalPrice - rentPrice;
+
+    if (profit > priceForExcursion)
+    {
+        Console.WriteLine($"Yes! {profit - priceForExcursion:F2} lv left.");
+    }
+    else
+    {
+        Console.WriteLine($"Not enough money! {priceForExcursion - profit:F2} lv needed.");
+    }
+}
+else
+{
+    double rentPrice = totalPrice * 0.10;
+    double profit = totalPrice - rentPrice;
+
+    if (profit > priceForExcursion)
+    {
+        Console.WriteLine($"Yes! {profit - priceForExcursion:F2} lv left.");
+    }
+    else
+    {
+        Console.WriteLine($"Not enough money! {priceForExcursion - profit:F2} lv needed.");
+    }
+}

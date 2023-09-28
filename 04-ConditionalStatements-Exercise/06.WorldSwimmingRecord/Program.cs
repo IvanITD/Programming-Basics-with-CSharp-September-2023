9dce5eebf1e1386a89d6a154366e4d2d9e890c3a
IvanITD <ivan78608@gmail.com>
2023-09-28 23:29:21 +0300

    Another resolved task 06. World Swimming Record

---
+++ 04-ConditionalStatements-Exercise/06.WorldSwimmingRecord/Program.cs
@@ -0,0 +1,18 @@
+﻿double recordInSeconds = double.Parse(Console.ReadLine());
+double distanceInMeters = double.Parse(Console.ReadLine());
+double timeInSeconds = double.Parse(Console.ReadLine());
+
+double swimmingTime = distanceInMeters * timeInSeconds;
+
+double delay = Math.Floor(distanceInMeters / 15);
+
+swimmingTime = swimmingTime + delay * 12.5;
+
+if (swimmingTime < recordInSeconds)
+{
+    Console.WriteLine($"Yes, he succeeded! The new world record is {swimmingTime:F2} seconds.");
+}
+else
+{
+    Console.WriteLine($"No, he failed! He was {Math.Abs(recordInSeconds - swimmingTime):F2} seconds slower.");
+}

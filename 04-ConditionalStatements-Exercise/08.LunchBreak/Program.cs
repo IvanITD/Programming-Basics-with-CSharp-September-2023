d1f399623d85cb5e65289510d92c736e00fde3fa
IvanITD <ivan78608@gmail.com>
2024-01-14 01:51:41 +0200

    8. Lunch Break Task Resolved

---
+++ 04-ConditionalStatements-Exercise/08.LunchBreak/Program.cs
@@ -0,0 +1,18 @@
+﻿string serialName = Console.ReadLine();
+int episodeLength = int.Parse(Console.ReadLine());
+int restLength = int.Parse(Console.ReadLine());
+
+double timeForLunch = restLength * 0.125;
+double timeForRest = restLength * 0.25;
+double remainingTime = restLength - timeForLunch - timeForRest;
+
+
+if (remainingTime >= episodeLength)
+{
+    Console.WriteLine($"You have enough time to watch {serialName} and left with {Math.Ceiling(remainingTime - episodeLength)} minutes free time.");
+}
+else if (remainingTime < episodeLength)
+{
+    
+    Console.WriteLine($"You don't have enough time to watch {serialName}, you need {Math.Ceiling(episodeLength - remainingTime)} more minutes.");
+}

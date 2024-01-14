e595236d614041dc32d10d7bea0db506156fc692
IvanITD <ivan78608@gmail.com>
2024-01-15 01:08:49 +0200

    07. Working Hours Another resolved task with the if else statement

---
+++ 05-ConditionalStatementsAdvanced-Lab/07.WorkingHours/Program.cs
@@ -0,0 +1,18 @@
+﻿int hour = int.Parse(Console.ReadLine());
+string dayFromWeek = Console.ReadLine();
+
+if (dayFromWeek == "Monday" || dayFromWeek == "Tuesday" || dayFromWeek == "Wednesday" || dayFromWeek == "Thursday" || dayFromWeek == "Friday" || dayFromWeek == "Saturday")
+{
+   if (hour >= 10 && hour <= 18)
+    {
+        Console.WriteLine("open");
+    }
+   else
+    {
+        Console.WriteLine("closed");
+    }
+}
+else if (dayFromWeek == "Sunday")
+{
+    Console.WriteLine("closed");
+}

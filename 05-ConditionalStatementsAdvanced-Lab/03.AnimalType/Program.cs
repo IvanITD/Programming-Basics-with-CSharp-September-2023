4f9877c2b6698b9c34de24d23968b4395111efb1
IvanITD <ivan78608@gmail.com>
2024-01-14 17:33:40 +0200

    03. Animal Type Task resolved with two different types of solution If else statement and switch case

---
+++ 05-ConditionalStatementsAdvanced-Lab/03.AnimalType/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+string animalType = Console.ReadLine();
+
+//This is the first way of solving the task with the if else statement
+
+if (animalType == "dog")
+{
+    Console.WriteLine("mammal");
+}
+else if (animalType == "crocodile" || animalType == "tortoise" || animalType == "snake")
+{
+    Console.WriteLine("reptile");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
+
+//This is the second way of solving the task with the switch case
+
+switch (animalType)
+{
+    case "dog":
+        Console.WriteLine("mammal");
+        break;
+
+    case "crocodile":
+    case "tortoise":
+    case "snake":
+        Console.WriteLine("reptile");
+        break;
+
+    default:
+        Console.WriteLine("unknown");
+        break;
+}

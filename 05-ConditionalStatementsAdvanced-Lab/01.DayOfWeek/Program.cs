29306c7fdf3a6b462bd130b2a7998a829d90d26f
IvanITD <ivan78608@gmail.com>
2024-01-14 16:49:33 +0200

    Added another way of resolving this task Resolved the task with the Switch case

--- 05-ConditionalStatementsAdvanced-Lab/01.DayOfWeek/Program.cs
+++ 05-ConditionalStatementsAdvanced-Lab/01.DayOfWeek/Program.cs
@@ -1,5 +1,8 @@
 ﻿int number = int.Parse(Console.ReadLine());
 
+
+//First way of solving this task is with the if else statement
+
 if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5 || number == 6 || number == 7)
 {
     if (number == 1)
@@ -38,4 +41,42 @@ if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5 || n
 else
 {
     Console.WriteLine("Error");
+}
+
+
+//The second way of solving this task is with the switch case
+
+switch (number)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+
+    default:
+        Console.WriteLine("Error");
+        break;
 }

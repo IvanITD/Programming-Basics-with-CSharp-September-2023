560c064cafb8f94362e78b3a7898be5262a8f4e5
IvanITD <ivan78608@gmail.com>
2024-01-14 16:30:49 +0200

    02. Weekend or Working Day Task resolved with two solutions If else statement & switch case

---
+++ 05-ConditionalStatementsAdvanced-Lab/02.WeekendorWorkDay/Program.cs
@@ -0,0 +1,41 @@
+﻿string dayOfWeek = Console.ReadLine();
+
+// Firstly we will write the solution of this task with the if else statement
+
+if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Wednesday" || dayOfWeek == "Thursday" || dayOfWeek == "Friday")
+{
+    Console.WriteLine("Working day");
+}
+else if (dayOfWeek == "Saturday" || dayOfWeek == "Sunday")
+{
+    Console.WriteLine("Weekend");
+}
+else
+{
+    Console.WriteLine("Error");
+}
+
+
+// The second solution is with the switch case
+
+switch (dayOfWeek)
+{
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+    case "Friday":
+        Console.WriteLine("Working day");
+        break;
+
+    case "Saturday":
+    case "Sunday":
+        Console.WriteLine("Weekend");
+        break;
+        default:
+        Console.WriteLine("Error");
+            break;
+
+
+        //For this task we resolved the problem with two different solutions
+}

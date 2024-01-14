ce358fb6167d5f59ea573b85702bb11becdd8fc5
IvanITD <ivan78608@gmail.com>
2024-01-15 00:28:46 +0200

    05.SmallShop Another task resolved with the if else statement!

---
+++ 05-ConditionalStatementsAdvanced-Lab/05.SmallShop/Program.cs
@@ -0,0 +1,97 @@
+﻿string product = Console.ReadLine();
+string city = Console.ReadLine();
+double quantity = double.Parse(Console.ReadLine());
+
+double coffee;
+double water;
+double beer;
+double sweets;
+double peanuts;
+
+// Here we solved the task using the if else statement
+
+if (city == "Sofia")
+{
+    if (product == "coffee")
+    {
+        coffee = 0.50;
+        quantity *= coffee;
+    }
+    else if (product == "water")
+    {
+        water = 0.80;
+        quantity *= water;
+    }
+    else if (product == "beer")
+    {
+        beer = 1.20;
+        quantity *= beer;
+    }
+    else if (product == "sweets")
+    {
+        sweets = 1.45;
+        quantity *= sweets;
+    }
+    else if (product == "peanuts")
+    {
+        peanuts = 1.60;
+        quantity *= peanuts;
+    }
+}
+else if (city == "Plovdiv")
+{
+    if (product == "coffee")
+    {
+        coffee = 0.40;
+        quantity *= coffee;
+    }
+    else if (product == "water")
+    {
+        water = 0.70;
+        quantity *= water;
+    }
+    else if (product == "beer")
+    {
+        beer = 1.15;
+        quantity *= beer;
+    }
+    else if (product == "sweets")
+    {
+        sweets = 1.30;
+        quantity *= sweets;
+    }
+    else if (product == "peanuts")
+    {
+        peanuts = 1.50;
+        quantity *= peanuts;
+    }
+}
+else if (city == "Varna")
+{
+    if (product == "coffee")
+    {
+        coffee = 0.45;
+        quantity *= coffee;
+    }
+    else if (product == "water")
+    {
+        water = 0.70;
+        quantity *= water;
+    }
+    else if (product == "beer")
+    {
+        beer = 1.10;
+        quantity *= beer;
+    }
+    else if (product == "sweets")
+    {
+        sweets = 1.35;
+        quantity *= sweets;
+    }
+    else if (product == "peanuts")
+    {
+        peanuts = 1.55;
+        quantity *= peanuts;
+    }
+}
+Console.WriteLine(quantity);

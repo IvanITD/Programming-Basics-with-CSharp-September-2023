1a0db57c5312ab364ebb53fd24484b9bf9f97efb
IvanITD <ivan78608@gmail.com>
2024-01-16 00:37:12 +0200

    01.Cinema Here we resolved the task with the if else statement

---
+++ 06-ConditionalStatementsAdvanced-Exercise/01.Cinema/Program.cs
@@ -0,0 +1,23 @@
+﻿string typeOfProjection = Console.ReadLine();
+int lineAmmount = int.Parse(Console.ReadLine());
+int columnsAmmount = int.Parse(Console.ReadLine());
+
+double result = 0.00;
+
+// Here we resolved the task with the if else statement
+if (typeOfProjection == "Premiere")
+{
+    double premierePrice = 12.00;
+    result = lineAmmount * columnsAmmount * premierePrice;
+}
+else if (typeOfProjection == "Normal")
+{
+    double normalPrice = 7.50;
+    result = lineAmmount * columnsAmmount * normalPrice;
+}
+else if (typeOfProjection == "Discount")
+{
+    double discountPrice = 5.00;
+    result = lineAmmount * columnsAmmount * discountPrice;
+}
+Console.WriteLine($"{result:F2} leva");

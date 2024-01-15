592eb0c4b389b04a5f47e98fa413c72d5293027b
IvanITD <ivan78608@gmail.com>
2024-01-15 02:49:53 +0200

    09. Fruit or Vegetable Here we solved the task with two different types of statements; if else & switch case statement

---
+++ 05-ConditionalStatementsAdvanced-Lab/09.FruitOrVegetable/Program.cs
@@ -0,0 +1,39 @@
+﻿string productName = Console.ReadLine();
+
+//Here we solved the assignment with the if else statement
+if (productName == "banana" || productName == "apple" || productName == "kiwi" || productName == "cherry" || productName == "lemon" || productName == "grapes")
+{
+    Console.WriteLine("fruit");
+}
+else if (productName == "tomato" || productName == "cucumber" || productName == "pepper" || productName == "carrot")
+{
+    Console.WriteLine("vegetable");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
+
+//Here we solved the assignment with the switch case statement
+switch (productName)
+{
+    case "banana":
+    case "apple":
+    case "kiwi":
+    case "cherry":
+    case "lemon":
+    case "grapes":
+        Console.WriteLine("fruit");
+        break;
+
+    case "tomato":
+    case "cucumber":
+    case "pepper":
+    case "carrot":
+        Console.WriteLine("vegetable");
+        break;
+
+    default:
+        Console.WriteLine("unknown");
+        break;
+}

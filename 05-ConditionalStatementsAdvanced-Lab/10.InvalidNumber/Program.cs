c7ed4ae4cbe5e12624fc3e4e2385407d4e85b2fa
IvanITD <ivan78608@gmail.com>
2024-01-15 02:54:42 +0200

    10. Invalid Number the assignment has been solved using the if else statement

---
+++ 05-ConditionalStatementsAdvanced-Lab/10.InvalidNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿int validNumber = int.Parse(Console.ReadLine());
+
+if (validNumber >= 100 && validNumber <= 200 || validNumber == 0)
+{
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine("invalid");
+}

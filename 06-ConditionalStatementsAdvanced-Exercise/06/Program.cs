da9e8ea7bc3c6cbb5a5ef7632d8cc11f6a87616f
Ivan Ivanov <ivan78608@gmail.com>
2024-01-19 00:40:54 +0200

    07. Hotel Room Another resolved task!

---
+++ 06-ConditionalStatementsAdvanced-Exercise/06/Program.cs
@@ -0,0 +1,66 @@
+﻿int N1 = int.Parse(Console.ReadLine());
+int N2 = int.Parse(Console.ReadLine());
+char symbol = char.Parse(Console.ReadLine());
+
+double result = 0.00;
+
+if (symbol == '+')
+{
+    result = N1 + N2;
+    if (result % 2 == 0)
+    {
+        Console.WriteLine($"{N1} + {N2} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{N1} + {N2} = {result} - odd");
+    }
+}
+else if (symbol == '-')
+{
+    result = N1 - N2;
+    if (result % 2 == 0)
+    {
+        Console.WriteLine($"{N1} - {N2} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{N1} - {N2} = {result} - odd");
+    }
+}
+else if (symbol == '*')
+{
+    result = N1 * N2;
+    if (result % 2 == 0)
+    {
+        Console.WriteLine($"{N1} * {N2} = {result} - even");
+    }
+    else
+    {
+        Console.WriteLine($"{N1} * {N2} = {result} - odd");
+    }
+}
+else if (symbol == '/')
+{
+    if (N1 == 0 || N2 == 0)
+    {
+        Console.WriteLine($"Cannot divide {N1} by zero");
+    }
+    else
+    {
+        result = (double)N1 / N2;
+        Console.WriteLine($"{N1} / {N2} = {result:F2}");
+    }
+}
+else if (symbol == '%')
+{
+    if (N1 == 0 || N2 == 0)
+    {
+        Console.WriteLine($"Cannot divide {N1} by zero");
+    }
+    else
+    {
+        result = N1 % N2;
+        Console.WriteLine($"{N1} % {N2} = {result}");
+    }
+}
---
+++ 06-ConditionalStatementsAdvanced-Exercise/07/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+
+string month = Console.ReadLine();
+int overnightStayCount = int.Parse(Console.ReadLine());
+
+double studio = 0.00;
+double apartment = 0.00;
+
+if (month == "May" || month == "October")
+{
+    studio = 50;
+    studio *= overnightStayCount; // Price per night!
+    apartment = 65;
+    apartment *= overnightStayCount; // Price per night!
+
+
+    if (overnightStayCount > 14)
+    {
+        studio -= (studio * 0.30); // За повече от 14 дни нощувки, намаление с 30%
+        apartment -= (apartment * 0.10); // За повече от 14 дни нощувки, намаление с 10%, без значение от месеца!
+    }
+    else if(overnightStayCount > 7)
+    {
+        studio -= (studio * 0.05); // За повече от 7 дни нощувка, намаление с 5%
+    }
+     
+}
+else if (month == "June" || month == "September")
+{
+    studio = 75.20;
+    studio *= overnightStayCount; // Price per night!
+    apartment = 68.70;
+    apartment *= overnightStayCount; // Price per night!
+
+    if (overnightStayCount > 14)
+    {
+        studio -= (studio * 0.20); // За повече от 14 дни нощувки, намаление с 20%
+        apartment -= (apartment * 0.10); // За повече от 14 дни нощувки, намаление с 10%, без значение от месеца!
+    }
+}
+else if (month == "July" || month == "August")
+{
+    studio = 76;
+    studio *= overnightStayCount; // Price per night!
+    apartment = 77;
+    apartment *= overnightStayCount; // Price per night!
+
+    if (overnightStayCount > 14)
+    {
+        apartment -= (apartment * 0.10); // За повече от 14 дни нощувки, намаление с 10%, без значение от месеца!
+    }
+}
+
+Console.WriteLine($"Apartment: {apartment:F2} lv.");
+Console.WriteLine($"Studio: {studio:F2} lv.");

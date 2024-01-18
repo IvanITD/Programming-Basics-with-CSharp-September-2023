b72466d40c9cf6975b0d50444e826d025e33d506
Ivan Ivanov <ivan78608@gmail.com>
2024-01-18 20:30:17 +0200

    05. Journey Another resolved task!

---
+++ 06-ConditionalStatementsAdvanced-Exercise/05/Program.cs
@@ -0,0 +1,51 @@
+﻿double budget = double.Parse(Console.ReadLine());
+string twoSeason = Console.ReadLine();
+
+string destination = "";
+string vacationType = "";
+double percentFromBudget = 0.00;
+
+if (twoSeason == "summer")
+{
+    if (budget <= 100)
+    {
+        destination = "Bulgaria";
+        vacationType = "Camp";
+        percentFromBudget = budget - (budget * 0.30);
+    }
+    else if (budget <= 1000)
+    {
+        destination = "Balkans";
+        vacationType = "Camp";
+        percentFromBudget = budget - (budget * 0.40);
+    }
+    else if (budget > 1000)
+    {
+        destination = "Europe";
+        vacationType = "Hotel";
+        percentFromBudget = budget - (budget * 0.90);
+    }
+}
+else if (twoSeason == "winter")
+{
+    if (budget <= 100)
+    {
+        destination = "Bulgaria";
+        vacationType = "Hotel";
+        percentFromBudget = budget - (budget * 0.70);
+    }
+    else if (budget <= 1000)
+    {
+        destination = "Balkans";
+        vacationType = "Hotel";
+        percentFromBudget = budget - (budget * 0.80);
+    }
+    else if (budget > 1000)
+    {
+        destination = "Europe";
+        vacationType = "Hotel";
+        percentFromBudget = budget - (budget * 0.90);
+    }
+}
+
+Console.WriteLine($"Somewhere in {destination} \n{vacationType} - {budget - percentFromBudget:F2}");
